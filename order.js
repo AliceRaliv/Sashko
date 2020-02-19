2ec0729ea87b513b3b629fd458f85de6813b6379
Sashko Dembelova <aliceraliv@yandex.ru>
2020-02-19 22:35:03 +0300

    работа checkbox

--- order.js
+++ order.js
@@ -1,17 +1,35 @@
-let select = "item1";
+let is_selected = false;
+let is_checkbox_selected = false;
+
 function switch_page(div_id){
+  if (is_selected == false){
+    error_print();
+  }
+  else{
+    display_page(div_id);
+  }
+}
+
+function back_page(div_id){
   display_page(div_id);
 }
 
 function display_page(div_id){
   const divs = document.getElementsByClassName("info");
+  error_clear();
   for(i = 0; i < divs.length; i++){
     if(divs[i].id == div_id){
       divs[i].style.display = 'grid';
+      is_selected = false;
       continue;
     }
-  divs[i].style.display = 'none';
+    divs[i].style.display = 'none';
   }
+
+}
+
+function error_print(){
+  document.getElementById("error").innerHTML = "Ошибка: Выберите пункт";
 }
 
 function error_clear(){
@@ -19,8 +37,30 @@ function error_clear(){
 }
 
 function item_choose(item_id){
-  item_clear_selection();
   const item = document.getElementById(item_id);
+
+  if(item_id == "stage6" && is_checkbox_selected == true){
+    item.style.background = "#FFFFFF";
+    is_checkbox_selected = false;
+    return "ok";
+  }
+
+  if(item_id == "stage1"){
+    document.getElementById("selectstage6").checked = false;
+  }
+
+  display_colorline(item_id);
+  error_clear();
+  if (item_id != "stage6"){
+    is_checkbox_selected = false;
+  }
+  else{
+    is_checkbox_selected = true;
+  }
+  if (is_checkbox_selected == false){
+    item_clear_selection();
+  }
+  is_selected = true;
   item.style.background = "#dfdfdf";
 }
 
@@ -34,4 +74,25 @@ function item_clear_selection(){
 function get_cost(){
   const cost = document.getElementById('cost').value;
   return cost;
+}
+
+function is_line(id){
+  if(id == "stage2" || id == "stage3" || id == "stage4" || id == "stage5" || id == "stage6"){
+    return true;
+  }
+
+  else{
+    return false;
+  }
+}
+
+function display_colorline(id){
+  const isline = is_line(id);
+  if (isline == true){
+    document.getElementById('stage6').style.display = 'block';
+  }
+  else{
+    document.getElementById('stage6').style.display = 'none';
+  }
+
 }
